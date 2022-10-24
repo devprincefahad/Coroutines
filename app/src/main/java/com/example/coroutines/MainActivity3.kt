e2bddf0efc474cf6377916553f13944c65c670c7
devprincefahad <1pfchouhan@gmail.com>
2022-10-24 06:36:49 +0530

    Coroutines

--- app/src/main/java/com/example/coroutines/MainActivity3.kt
+++ app/src/main/java/com/example/coroutines/MainActivity3.kt
@@ -2,6 +2,8 @@ package com.example.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
 
 class MainActivity3 : AppCompatActivity() {
 
@@ -11,5 +13,48 @@ class MainActivity3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            printFollowers()
+        }
+
     }
+
+    private suspend fun printFollowers() {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            var fb = async { getFbFollowers() }
+            var insta = async { getInstaFollowers() }
+            Log.d(TAG, "FB - ${fb.await()}, Insta - ${insta.await()}")
+        }
+
+        /*  val fb = CoroutineScope(Dispatchers.IO).async {
+              getFbFollowers()
+          }
+
+          val insta = CoroutineScope(Dispatchers.IO).async {
+              getInstaFollowers()
+          }
+
+          Log.d(TAG, "FB - ${fb.await()}, Insta - ${insta.await()}")*/
+
+        /*val job = CoroutineScope(Dispatchers.IO).async {
+            getFbFollowers()
+            "Hello"
+        }
+        Log.d(TAG, job.await().toString())
+        */
+
+
+    }
+
+    private suspend fun getFbFollowers(): Int {
+        delay(1000)
+        return 54
+    }
+
+    private suspend fun getInstaFollowers(): Int {
+        delay(1000)
+        return 113
+    }
+
 }
